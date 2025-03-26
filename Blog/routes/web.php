01f0d5b8da5da00d142fe16bf12789390612bdb4
Aminebennani1 <mohammedaminebennanikabchi@gmail.com>
2025-03-26 12:04:54 +0000

    solution

--- Blog/routes/web.php
+++ Blog/routes/web.php
@@ -2,15 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 use App\Http\Controllers\ArticleController;
 
 Route::get('/', [ArticleController::class, 'index'])->name('articles.index');
-Route::get('/articles/{id}', [ArticleController::class, 'show'])->name('articles.show');
+Route::get('/articles/create', [ArticleController::class, 'create'])->name('articles.create');
 Route::post('/articles', [ArticleController::class, 'store'])->name('articles.store');
+Route::get('/articles/{id}', [ArticleController::class, 'show'])->name('articles.show');
+
+
 
 
 
